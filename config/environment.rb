b6f3b18195175a6e983aba80227e6176b4534796
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-08 09:45:56 +0000

    add /plugins directory

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,7 @@ Rails::Initializer.run do |config|
 
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
+  config.plugin_paths += %W( #{RAILS_ROOT}/plugins )
 
   # Force all environments to use the same logger level
   # (by default production uses :info, the others :debug)

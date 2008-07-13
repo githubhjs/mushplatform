2dfa17fd5580f07db7c39b28a02f68e7ee47c328
RainLiu67 <RainLiu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-13 14:04:08 +0000

    impletment scriptlet

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20080713095646) do
+
+  create_table "menubars", :force => true do |t|
+    t.string "menu_name"
+    t.string "menu_link"
+  end
 
 end

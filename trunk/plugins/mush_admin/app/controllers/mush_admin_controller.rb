47852ca3c3aaf6e0237013377c47aa8e6f461abd
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-15 13:46:10 +0000

    change mush admin plugin's some assets directory

--- trunk/plugins/mush_admin/app/controllers/mush_admin_controller.rb
+++ trunk/plugins/mush_admin/app/controllers/mush_admin_controller.rb
@@ -3,6 +3,6 @@ class MushAdminController < ApplicationController
   layout 'admin'
   def index
     @extension_menus = MushAdmin::MushAdminExtension.execute_extension(MushAdmin::MushAdminExtension.add_more_menu)
-    render :template => '/mush_admin/index'
+    render :template => 'mush_admin/index'
   end
 end

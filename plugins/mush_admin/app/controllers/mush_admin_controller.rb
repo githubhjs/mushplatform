3b69e9c44add26eabbaf6461b86253e75db60657
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-16 06:34:31 +0000

    refactor extensions

--- plugins/mush_admin/app/controllers/mush_admin_controller.rb
+++ plugins/mush_admin/app/controllers/mush_admin_controller.rb
@@ -3,7 +3,7 @@ require 'mush_admin/extension_points'
 class MushAdminController < ApplicationController
   layout 'admin'
   def index
-    @extension_menus = MushAdmin::ExtensionPoints.execute_extension(MushAdmin::ExtensionPoints.add_more_menu)
+    @extension_menus = MushAdmin::ExtensionPoints.execute_extension('add_more_menu')
     render :template => 'mush_admin/index'
   end
 end

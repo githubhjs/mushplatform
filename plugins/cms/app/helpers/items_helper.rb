4cbb637e0d83a6d14cd87576524a9ab11de1cbf7
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-16 10:36:57 +0000

    add parameters supported in scriptlet

--- plugins/cms/app/helpers/items_helper.rb
+++ plugins/cms/app/helpers/items_helper.rb
@@ -1,11 +1,11 @@
 module ItemsHelper
-  def list_article
-    {'articles' => ["article one", "article two"]}
+  def list_article(args = {})
+    {'articles' => ["article one", "article two"], 'limit' => args[:limit]}
   end
     
-  def articles
+  def articles_template
     "{% for article in articles %}" +
     " <a href='#'>article</a> " +
-    "{% endfor %}"
+    "{% endfor %}" + "limit : {{limit}} <br/>"
   end
 end
--- plugins/cms/lib/scriptlets.rb
+++ plugins/cms/lib/scriptlets.rb
@@ -4,5 +4,5 @@ include Mush::Scriptlets
 require 'items_helper'
 include ItemsHelper
   
-add_scriptlet('article_list_by_all', :list_article, articles)
+add_scriptlet('article_list_by_all', :list_article, articles_template)
 add_scriptlet('article_list_by_tag', :list_article)

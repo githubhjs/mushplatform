b625f4fcdc4c0b9cf06393e73df4bff63c15d813
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-17 10:49:21 +0000

    refine extension and extension points

--- trunk/plugins/cms/app/helpers/admin/admin_helper.rb
+++ trunk/plugins/cms/app/helpers/admin/admin_helper.rb
@@ -4,18 +4,26 @@
 module AdminHelper
   
   class << self
-    def add_menu_to_bar
-      menu_bar = <<menu
-<li><a href="#nogo">CMS &#187;<!--[if gte IE 7]><!--></a><!--<![endif]-->
-    <!--[if lte IE 6]><table><tr><td><![endif]--><ul>
-            <li><a href="/admin/templates">Templates</a></li>
-            <li><a href="#nogo">Columns</a></li>
-            <li><a href="#nogo">Aritcles</a></li>
-    </ul><!--[if lte IE 6]></td></tr></table></a><![endif]-->
-</li>
+    def add_mainmenu
+      <<menu
+  <li><a href="#nogo">CMS &#187;<!--[if gte IE 7]><!--></a><!--<![endif]-->
+      <!--[if lte IE 6]><table><tr><td><![endif]--><ul>
+              <li><a href="/admin/templates">Templates</a></li>
+              <li><a href="#nogo">Columns</a></li>
+              <li><a href="#nogo">Aritcles</a></li>
+      </ul><!--[if lte IE 6]></td></tr></table></a><![endif]-->
+  </li>
 menu
       menu_bar
     end  
+    
+    def add_submenu
+      <<menu
+  <li><a href="/admin/templates">Templates</a></li>
+  <li><a href="#nogo">Columns</a></li>
+  <li><a href="#nogo">Aritcles</a></li>
+menu
+    end
   end
   
 end
--- trunk/plugins/cms/lib/extensions.rb
+++ trunk/plugins/cms/lib/extensions.rb
@@ -5,4 +5,5 @@ require 'admin/admin_helper'
 include AdminHelper
 
 # params is extension_point, class, method
-add_extension('mush_admin_add_more_menu', AdminHelper, 'add_menu_to_bar')
+add_extension('mush_admin_add_mainmenu', AdminHelper, 'add_mainmenu')
+add_extension('mush_admin_add_submenu', AdminHelper, 'add_submenu')

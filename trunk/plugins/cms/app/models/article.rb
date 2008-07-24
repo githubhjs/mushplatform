04348bf59e989bc7342e4dec12da06ea8fc4b97f
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-24 08:43:56 +0000

    add manage articles

--- trunk/plugins/cms/app/models/article.rb
+++ trunk/plugins/cms/app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ActiveRecord::Base
+  partial_updates = true
+  belongs_to :channel
+  named_scope :by_channel, lambda {|id| {:conditions => "channel_id = #{id}", :order => "created_at DESC"}}
+  
 end

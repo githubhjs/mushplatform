155edd53a456895657dd1247a63a16bd66b0e496
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-25 23:59:19 +0000

    fix engines migrator bug to be compatible with rails 2.1

--- trunk/plugins/authorize/db/migrate/20080718130351_create_users.rb
+++ trunk/plugins/authorize/db/migrate/20080718130351_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   
   def self.up
-    create_table :users do |t|
+    create_table :users, :force => true do |t|
       t.string   :user_name,:hashed_password,:email,:salt,:null => false
       t.datetime :created_at
       t.integer  :status ,:null => false,:default => 0

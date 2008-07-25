e24e1dddd732c0fdc165f85131a56f93542c89e1
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-25 15:51:02 +0000

    add display article detail page, using id or permalink

--- plugins/cms/app/controllers/cms_controller.rb
+++ plugins/cms/app/controllers/cms_controller.rb
@@ -7,17 +7,56 @@ class CmsController < ApplicationController
   end
   
   def dispatch
-    channel = Channel.find_by_permalink("/#{recognize_permalink(params[:path])}")
+    path = params[:path]
+    if path[path.length - 2] == 'article'
+      channel_layout, content = recognize_article(path)
+    else
+      channel_layout, content = recognize_channel(path)
+    end
+    render :text => channel_layout ? Liquid::Template.parse(channel_layout).render('content' => content) : 'Page Not Found'
+  end
+  
+  def recognize_channel(path)
+    channel = Channel.find_by_permalink("/#{recognize_permalink(path)}")
+    return nil, nil unless channel
     if channel.template_id
       channel_layout = channel.template.body
       content = Liquid::Template.parse(channel.body).render
     else
       channel_layout = channel.body
     end
-    render :text => channel ? Liquid::Template.parse(channel_layout).render('content' => content) : 'Page Not Found'
+    return channel_layout, content
+  end
+
+  def recognize_article(path)
+    begin
+      case id = path.last
+      when /^(\d*)$/
+        article = Article.find(id)
+      else
+        article = Article.find_by_permalink(id)
+      end
+    rescue
+      # if couldn't find article, using channel's 
+      # template to render 'Article Not Found'
+      channel_layout, content = recognize_channel(path[0, path.length - 2])
+      content = 'Article Not Found'
+      return channel_layout, content
+    end
+    channel = article.channel
+    if channel.template_id
+      channel_layout = channel.template.body
+    else
+      channel_layout = channel.body
+    end
+    template = Template.find_by_name('Article')
+    article_template = template.body if template
+    article_template = Liquid::Template.file_system.read_template_file('article') unless article_template
+    content = Liquid::Template.parse(article_template).render('article' => article)
+    return channel_layout, content
   end
   
   def recognize_permalink(path)
-    full_path = path.join('/')
+    path.join('/')
   end  
 end
--- plugins/cms/db/migrate/20080719064435_init_cms.rb
+++ plugins/cms/db/migrate/20080719064435_init_cms.rb
@@ -10,10 +10,10 @@ class InitCms < ActiveRecord::Migration
       t.timestamps
     end
     index = Channel.create(:name => 'index', :permalink => '/', :body => '[[article_list_by_all(limit=3000)]]', :template_id => 1)
-    index.add_child(news = Channel.create(:name => 'news', :permalink => '/news'))
+    index.add_child(news = Channel.create(:name => 'news', :permalink => '/news', :template_id => 1))
     index.add_child(vendors = Channel.create(:name => 'vendors', :permalink => '/vendors'))
-    news.add_child(Channel.create(:name => 'general', :permalink => '/news/gneral'))
-    news.add_child(Channel.create(:name => 'special', :permalink => '/news/special'))
+    news.add_child(Channel.create(:name => 'general', :permalink => '/news/general', :template_id => 1))
+    news.add_child(Channel.create(:name => 'special', :permalink => '/news/special', :template_id => 1))
     vendors.add_child(Channel.create(:name => 'google', :permalink => '/vendors/google'))
     vendors.add_child(Channel.create(:name => 'yahoo', :permalink => '/vendors/yahoo'))
     
@@ -23,11 +23,12 @@ class InitCms < ActiveRecord::Migration
       t.text :body
       t.timestamps
     end
-    Template.create(:name => 'Default Layout', :body => '<div>HEADER</div><div>{{content}}</div><div>FOOTER</div>')
+    Template.create(:name => 'Default Layout', :category => 'Layout', :body => '<div>HEADER</div><div>{{content}}</div><div>FOOTER</div>')
+    Template.create(:name => 'Article', :category => 'Template', :body => '<h2>{{article.title}}</h2><h4>{{article.subtitle}}</h4><p>{{article.author}}</p><div>{{article.body}}</div><p>{{article.author}}</p><p>Template from Database</p>')
     
     create_table "articles", :force => true do |t|
       t.string :title, :null => false
-      t.string :url, :subtitle, :display_title, :author, :origin, :image, :file
+      t.string :url, :subtitle, :display_title, :permalink, :author, :origin
       t.text :body, :excerpt
       t.integer :status
       t.integer :channel_id

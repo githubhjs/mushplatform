17aa21c3ec8d40a05586ffe5beba28163c1a5fa4
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-25 15:51:02 +0000

    add display article detail page, using id or permalink

--- trunk/plugins/cms/app/models/article.rb
+++ trunk/plugins/cms/app/models/article.rb
@@ -2,5 +2,8 @@ class Article < ActiveRecord::Base
   partial_updates = true
   belongs_to :channel
   named_scope :by_channel, lambda {|id| {:conditions => "channel_id = #{id}", :order => "created_at DESC"}}
-  
+
+  def to_liquid
+     self.attributes.stringify_keys
+  end   
 end

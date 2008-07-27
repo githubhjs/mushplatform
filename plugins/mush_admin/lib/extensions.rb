627d1d5e7704088dd979c06ef69a58ac25ceb1bb
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-27 04:27:17 +0000

    refine some style

--- plugins/mush_admin/lib/extensions.rb
+++ plugins/mush_admin/lib/extensions.rb
@@ -5,4 +5,4 @@ require 'admin/mush_admin_helper'
 include MushAdminHelper
 
 # params is extension_point, class, method, priority
-add_extension('MushAdmin::AddMainmenu', MushAdminHelper, 'add_mainmenu')
+add_extension('MushAdmin::AddMainmenu', MushAdminHelper, 'add_mainmenu', 9)

e9271e2d6b696e3485ee429e9ad698377431ef7f
RainLiu67 <RainLiu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-07-28 15:11:45 +0000

    add crawler plugin

--- trunk/db/migrate/20080720032857_authorize_to_version_20080720032627.rb
+++
@@ -1,9 +0,0 @@
-class AuthorizeToVersion20080720032627 < ActiveRecord::Migration
-  def self.up
-    Engines.plugins["authorize"].migrate(20080720032627)
-  end
-
-  def self.down
-    Engines.plugins["authorize"].migrate(0)
-  end
-end

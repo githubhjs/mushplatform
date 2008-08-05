6808b691fec0f36cf4b1a346a13b432224781e31
RainLiu67 <RainLiu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-08-05 03:40:53 +0000

    add login

--- trunk/app/controllers/signup_controller.rb
+++ trunk/app/controllers/signup_controller.rb
@@ -14,7 +14,7 @@ class SignupController < ApplicationController
       @profile.user_id = @user.id
       if @profile.save  
         flash[:notice] = 'UserProfile was successfully created.'
-        render :template => '/signup/login'
+        render :template => '/login/login'
         return true
       end
     end
@@ -22,31 +22,11 @@ class SignupController < ApplicationController
     return true
   end
   
-  def login
-    unless request.post?
-      render :template => '/signup/login'
-      retun true
-    end
-    session[:user] = User.authenticate(params[:user][:user_name],params[:user][:password])
-    if session[:user]
-      flash[:message]  = "Login successful"
-      render :text => "Login successful"
-    else
-      flash[:warning] = "Login unsuccessful"
-    end
-  end
-
-  def logout
-    session[:user] = nil
-    flash[:message] = 'Logged out'
-    redirect_to :action => 'login'
-  end
-  
   def forgot_password
     u= User.find_by_email(params[:user][:email])
     if u and u.send_new_password
       flash[:message]  = "A new password has been sent by email."
-      redirect_to :action=>'login'
+      redirect_to '/login'
     else
       flash[:warning]  = "Couldn't send password"
     end

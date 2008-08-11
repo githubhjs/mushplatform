421eaa7615f1a418977d44386a81410873d09f15
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-08-11 14:56:31 +0000

    add channel_id when crawler articles generate articles

--- plugins/crawler/app/models/crawler_article.rb
+++ plugins/crawler/app/models/crawler_article.rb
@@ -6,10 +6,11 @@ class CrawlerArticle < ActiveRecord::Base
     article = Article.new
     article.title = self.title
     article.author = self.author
-    article.subtitle = self.summary
+    article.subtitle = self.title
     article.display_title = self.summary
     article.crawler_article_id = self.id
     article.url = self.source_url
+    article.channel_id = 1
     article.save
   end
 end

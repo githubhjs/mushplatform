3724025e73f957f1595c7f11d97adf727889f304
rainliu67 <rainliu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-08-26 13:08:03 +0000

    delete blogengine

--- plugins/blogengine/init.rb
+++
@@ -1,3 +0,0 @@
-# Include hook code here
-#require "patches/controller_ex.rb"
-Theme.copy_themes_to_public
--- plugins/blogengine/routes.rb
+++
@@ -1,7 +0,0 @@
-map.with_options(:conditions => {:method => :get}) do |get|  
-  get.with_options(:controller => 'themes', :filename => /.*/, :conditions => {:method => :get}) do |theme|  
-    theme.connect 'stylesheets/theme/:filename', :action => 'stylesheets'  
-    theme.connect 'javascripts/theme/:filename', :action => 'javascript'  
-    theme.connect 'images/theme/:filename', :action => 'images'  
-  end  
-end  

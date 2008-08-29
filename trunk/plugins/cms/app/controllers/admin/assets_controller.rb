37a0e1380e8a1f7a95c859fdfa80736f581b922c
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-08-29 07:36:05 +0000

    change asset from db implementation to local fs

--- trunk/plugins/cms/app/controllers/admin/assets_controller.rb
+++ trunk/plugins/cms/app/controllers/admin/assets_controller.rb
@@ -1,3 +1,6 @@
+require 'shared-mime-info'
+require 'zip/zipfilesystem'
+
 class Admin::AssetsController < ApplicationController
   layout 'admin'
   BASE_PATH = "#{RAILS_ROOT}/public/assets"
@@ -5,34 +8,37 @@ class Admin::AssetsController < ApplicationController
   # GET /assets
   # GET /assets.xml
   def index
-    @assets = Asset.paginate :page => params[:page], :order => 'created_at DESC'
-    start_path = (params[:path] and params[:path].length > 0) ? "#{BASE_PATH}/#{params[:path]}" : BASE_PATH
+    @path = params[:path]
+    start_path = (@path and @path.length > 0) ? "#{BASE_PATH}/#{@path}" : BASE_PATH
     start_path = BASE_PATH if start_path == "#{BASE_PATH}/.."
     start_path = BASE_PATH if start_path == "#{BASE_PATH}/."
     @assets =  Dir.entries(start_path).sort.collect { |path|
+      # file absolute path
       absolute_path = "#{start_path}/#{path}"
-#      relative_path = params[:path] ? absolute_path[start_path.index(params[:path]), absolute_path.length-1] : path
       
       # file relative path
-#      relative_path = (params[:path] and params[:path].length > 0) ? path : "#{params[:path]}/#{path}"
       relative_path = absolute_path[BASE_PATH.length+1, absolute_path.length]
         
-      # file absolute path
-#      absolute_path = "#{BASE_PATH}/#{relative_path}"
-      
       # file parent directory path
       if relative_path.index('/..')
         parent_path = relative_path[0,relative_path.length-3]
         parent_path = parent_path.split('/')
         parent_path.delete(parent_path.last)
         parent_path = parent_path.join('/')
+        relative_path = parent_path
       elsif relative_path.index('/.')
-        parent_path = relative_path[0,relative_path.length-2] 
+        parent_path = relative_path[0,relative_path.length-2]
+        relative_path = parent_path
       else
         parent_path = relative_path
       end
       
-      Asset.new(:filename => File.basename(absolute_path), :name => absolute_path, :path => parent_path, :created_at => File.atime(absolute_path))
+      Asset.new(:filename => File.basename(absolute_path), 
+                :path => relative_path,
+                :full_path => absolute_path, 
+                :parent_path => parent_path,
+                :content_type => MIME.check(absolute_path).to_s,
+                :created_at => File.atime(absolute_path))
     }
 
     respond_to do |format|
@@ -43,7 +49,9 @@ class Admin::AssetsController < ApplicationController
   # GET /assets/1
   # GET /assets/1.xml
   def show
-    @asset = Asset.find(params[:id])
+    file = params[:file]
+    @asset = Asset.new(:filename =>file)
+    @body = IO.read("#{BASE_PATH}/#{file}")
 
     respond_to do |format|
       format.html # show.html.erb
@@ -54,7 +62,7 @@ class Admin::AssetsController < ApplicationController
   # GET /assets/new
   # GET /assets/new.xml
   def new
-    @asset = Asset.new
+    @path = params[:path]
 
     respond_to do |format|
       format.html # new.html.erb
@@ -64,53 +72,95 @@ class Admin::AssetsController < ApplicationController
 
   # GET /assets/1/edit
   def edit
-    @asset = Asset.find(params[:id])
+    path = params[:path]
+    full_path = "#{BASE_PATH}/#{path}"
+    @asset = Asset.new(
+                :filename => File.basename(path), 
+                :path => path,
+                :full_path => full_path, 
+                :parent_path => path.gsub(/\/([^\/]*)\.([^\/]*)/, ''),
+                :content_type => MIME.check(full_path).to_s,
+                :created_at => File.atime(full_path))
+    @body = IO.read(@asset.full_path)
   end
 
   # POST /assets
   # POST /assets.xml
   def create
-    @asset = Asset.new(params[:asset])
+    @path = params[:path]
+    dir = params[:dir]
+    file = params[:file]
+    
+    full_dir = (dir and dir.length > 0) ? "#{BASE_PATH}/#{@path}/#{dir}" : "#{BASE_PATH}/#{@path}"
+    FileUtils.mkdir_p(full_dir)
+    
+    if file.class != String
+      filename = file.original_filename
+      full_path = "#{full_dir}/#{filename}"
+      File.open(full_path, "w") { |file| file.write(params[:file].read) }
+      unzip_file(full_path, full_dir) if MIME.check(full_path).subtype == 'zip'
+      File.delete full_path
+    end
 
     respond_to do |format|
-      if @asset.save
-        flash[:notice] = 'Asset was successfully created.'
-        format.html { redirect_to(assets_url) }
-        format.xml  { render :xml => @asset, :status => :created, :location => @asset }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @asset.errors, :status => :unprocessable_entity }
-      end
+      flash[:notice] = 'Asset was successfully created.'
+      format.html { redirect_to(:action => 'index', :path => @path ) }
+      format.xml  { render :xml => @asset, :status => :created, :location => @asset }
     end
   end
 
   # PUT /assets/1
   # PUT /assets/1.xml
   def update
-    @asset = Asset.find(params[:id])
-
+    path = params[:path]
+    full_path = "#{BASE_PATH}/#{path}"
+    @asset = Asset.new(
+                :filename => File.basename(path), 
+                :path => path,
+                :full_path => full_path, 
+                :parent_path => path.gsub(/\/([^\/]*)\.([^\/]*)/, ''),
+                :content_type => MIME.check(full_path).to_s,
+                :created_at => File.atime("#{BASE_PATH}/#{path}"))    
+    File.open(full_path,"w"){|file| file.puts params[:body]}
+    @body = IO.read(full_path)
+    
     respond_to do |format|
-      if @asset.update_attributes(params[:asset])
-        flash[:notice] = 'Asset was successfully updated.'
-        format.html { redirect_to(assets_url) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @asset.errors, :status => :unprocessable_entity }
-      end
+      flash[:notice] = 'Asset was successfully updated.'
+      format.html { render :template => 'admin/assets/edit' }
+      format.xml  { head :ok }
     end
   end
 
   # DELETE /assets/1
   # DELETE /assets/1.xml
   def destroy
-    params[:id].each{|id| 
-      Asset.find(id.to_i).destroy
-    } if params[:id]
+    params[:path].each{|path|
+      full_path = "#{BASE_PATH}/#{path[0]}"
+      begin
+        if File.directory?(full_path)
+          #Dir.rmdir full_path
+          FileUtils.rm_rf(full_path)
+        else
+          File.delete full_path
+        end
+      rescue
+      end
+    } if params[:path]
 
     respond_to do |format|
       format.html { redirect_to(assets_url) }
       format.xml  { head :ok }
     end
   end
+  
+  def unzip_file(source, target)
+    Zip::ZipFile.open(source) do |zipfile|
+     zipfile.each { |entry|
+       full_path = File.join(target, entry.name)
+       FileUtils.mkdir_p(File.dirname(full_path))
+       zipfile.extract(entry, full_path) unless File.exist?(full_path)
+     }
+    end
+  end
+  
 end
--- trunk/plugins/cms/app/models/asset.rb
+++ trunk/plugins/cms/app/models/asset.rb
@@ -1,8 +1,9 @@
 class Asset < CachedModel
-  has_attachment :storage => :file_system
+#  has_attachment :storage => :file_system
+  attr_accessor :filename, :path, :full_path, :parent_path, :content_type, :created_at
   
   def dir?
-    File.directory?(name)
+    File.directory?(full_path)
   end
   
   def filename_for_index

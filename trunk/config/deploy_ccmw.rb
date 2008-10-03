8467df654b51305eabb1d4878c1887679c75645b
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-03 23:21:45 +0000

    add restart web server after update from svn

--- trunk/config/deploy_ccmw.rb
+++ trunk/config/deploy_ccmw.rb
@@ -20,6 +20,7 @@ role :db,  "www.ccmw.net", :primary => true
 after "deploy:setup", "db:create_database_yml"
 after "deploy:update_code", "db:symlink_database_yml"
 after "deploy:symlink", "deploy:change_owner" 
+after "deploy:svn_update", "deploy:restart" 
 
 namespace "db" do
   

00860f0e12e4ae54adf052acf7176bba7772837c
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-03 23:34:54 +0000

    add model to_liquid

--- plugins/ccmw/app/models/link.rb
+++ plugins/ccmw/app/models/link.rb
@@ -3,5 +3,8 @@ class Link < CachedModel
   def before_create
     self.category = 'N/A' if self.category.empty?
   end  
-  
+
+  def to_liquid
+     self.attributes.stringify_keys
+  end  
 end
--- plugins/ccmw/init.rb
+++ plugins/ccmw/init.rb
@@ -3,3 +3,9 @@ require 'ccmw'
 Article.class_eval do
   belongs_to :category, :class_name => 'ArticleCategory', :foreign_key => 'category_id'
 end
+
+Tag.class_eval do
+  def to_liquid
+     self.attributes.stringify_keys
+  end  
+end

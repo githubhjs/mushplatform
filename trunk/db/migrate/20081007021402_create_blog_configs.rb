d7b53b93a2230d267d8c55f151b20ffd7217b7b7
RainLiu67 <RainLiu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-07 02:40:18 +0000

    add blog_config

---
+++ trunk/db/migrate/20081007021402_create_blog_configs.rb
@@ -0,0 +1,15 @@
+class CreateBlogConfigs < ActiveRecord::Migration
+  def self.up
+    create_table :blog_configs, :force => true, :options => "ENGINE=MyISAM DEFAULT CHARSET=utf8" do |t|
+      t.integer :user_id,:null => false
+      t.string  :blog_name
+      t.integer :blog_perpage,:default => MySpaceController::Blog_Count_PerPage
+      t.integer :comment_perpage,:default => MySpaceController::Comment_Count_PerPage
+      t.string  :theme_name,:default => Theme::Default_Theme_Name
+    end
+  end
+
+  def self.down
+    drop_table :blog_configs
+  end
+end

bb5843d62d30ebb4e3d1e579d10fff1fa770cdc1
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-08 13:31:20 +0000

    add links image upload directory

--- trunk/plugins/ccmw/app/models/link.rb
+++ trunk/plugins/ccmw/app/models/link.rb
@@ -1,5 +1,5 @@
 class Link < CachedModel
-  has_attachment :storage => :file_system, :content_type => :image
+  has_attachment :storage => :file_system, :content_type => :image, :path_prefix => 'public/assets/images'
   
   def before_create
     self.category = 'N/A' if self.category.empty?

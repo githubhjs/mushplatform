5502a7559314b0b036750e6f76f09e033daee0d0
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-09 10:05:20 +0000

    add sweep script

--- trunk/plugins/cms/app/models/site_sweeper.rb
+++ trunk/plugins/cms/app/models/site_sweeper.rb
@@ -1,5 +1,5 @@
 class SiteSweeper < ActionController::Caching::Sweeper
-  observe Article, Channel
+  observe Article, Channel, Template
 
   def after_save(record)
     self.class::sweep
@@ -9,10 +9,10 @@ class SiteSweeper < ActionController::Caching::Sweeper
     self.class::sweep
   end
   
-  def self.sweep
+  def self.sweep(pattern = "*")
     cache_dir = ActionController::Base.page_cache_directory
     unless cache_dir == RAILS_ROOT+"/public"
-      FileUtils.rm_r(Dir.glob(cache_dir+"/*")) rescue Errno::ENOENT
+      FileUtils.rm_r(Dir.glob(cache_dir+"/#{pattern}")) rescue Errno::ENOENT
       RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully swept.")
     end
   end

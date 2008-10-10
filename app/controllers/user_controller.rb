fa2119566b56c655fde85e982a09813a3095401a
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-10 00:41:07 +0000

    extract all login and sign action into user_controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,95 @@
+class UserController < ApplicationController
+  
+  layout 'site'
+  skip_before_filter :verify_authenticity_token
+  
+  def select
+  end
+  
+  def signup
+    @user = User.new(params[:user])
+    @profile = UserProfile.new(params[:profile])
+    return if generate_blank
+    if @user.save  
+      @profile.user_id = @user.id
+      if @profile.save  
+        flash[:notice] = 'UserProfile was successfully created.'
+        render :template => 'login'
+        return true
+      end
+    end
+    render :template => "/signup/index"
+    return true
+  end
+  
+  def login
+    # if get the login page, just render
+    return if generate_blank
+
+    # unless, the request is actually login
+    session[:user] = params[:user].blank? ? nil : User.authenticate(params[:user][:user_name],params[:user][:password])
+    if session[:user]
+      flash[:message]  = "Login successful"
+      #redirect_to session[:return_to] || "/admin" 
+      unless params[:admin].blank?
+        redirect_to session[:return_to] || "/admin"
+      else
+        redirect_to session[:return_to] || params[:forward] || "/"
+      end
+    else
+      flash[:warning] = "Login unsuccessful"
+      render :layout => get_layout
+    end    
+  end
+  
+  def login_bar
+    render :layout => 'login'
+  end  
+
+  def logout
+    session[:user] = nil
+    session[:return_to] = nil
+    flash[:message] = 'Logged out'
+    if params['page'] == 'index'
+      redirect_to :controller => 'user', :action => 'login_bar'
+    else
+      redirect_to :controller => 'user', :action => 'login'
+    end
+  end
+  
+  def forgot_password
+    u= User.find_by_email(params[:user][:email])
+    if u and u.send_new_password
+      flash[:message]  = "A new password has been sent by email."
+      redirect_to '/login'
+    else
+      flash[:warning]  = "Couldn't send password"
+    end
+  end
+
+  def change_password
+    @user=session[:user]
+    if request.post?
+      @user.update_attributes(:password=>params[:user][:password], :password_confirmation => params[:user][:password_confirmation])
+      if @user.save
+        flash[:message]="Password Changed"
+      end
+    end
+  end
+
+  # Generate a template user for certain actions on get
+  def generate_blank
+    case request.method
+    when :get
+      @user = User.new
+      render :layout => get_layout
+      return true
+    end
+    return false
+  end
+  
+  def get_layout
+    params[:admin].blank? ? "site" : false
+  end
+  
+end

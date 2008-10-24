3f3876a6548b0b9ed674d1f113805db1526b1343
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-10-24 04:14:26 +0000

    add latest entries limit

--- sidebars/latest_article/helpers/latest_article_sidebar_helper.rb
+++ sidebars/latest_article/helpers/latest_article_sidebar_helper.rb
@@ -1,10 +1,10 @@
 module LatestArticleSidebarHelper
   
-  Default_Latest_Article_Perpage = 5
+  Default_Latest_Article_Perpage = 10
 
   def self.get_context(option = {})
     Blog.find(:all,:conditions => "published = #{Blog::Published_Blogs} and user_id=#{option[:user_id]}",
-    :order => "updated_at desc")
+    :order => "updated_at desc", :limte => Default_Latest_Article_Perpage)
   end
 
   def self.get_edit_context(options = {})

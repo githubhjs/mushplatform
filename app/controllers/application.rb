21a9215aaf634356b30a9eea78df8fc7638d5343
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-11-03 09:31:48 +0000

    add i18n

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,6 +4,15 @@ require 'auth/auth'
 class ApplicationController < ActionController::Base
   
   helper :all # include all helpers, all the time
+  before_filter :set_locale
+
+  def set_locale
+      e = request.env['HTTP_ACCEPT_LANGUAGE']
+      # update session if passed
+      session[:locale] = params[:locale] if params[:locale]
+      # set locale based on session or default
+      I18n.locale = session[:locale] || I18n.default_locale
+  end
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -6,3 +6,6 @@
 #  File.basename(File.basename(locale_file, ".rb"), ".yml")
 #end + ['en-US']).uniq
 #locale_files.each { |locale_file| I18n.load_translations locale_file }
+#I18n.default_locale = 'en-US'
+I18n.default_locale = 'en-US'
+#I18n.locale         = 'en-US'

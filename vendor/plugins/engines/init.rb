6c53dea18161d6fdbaa85b174b4cefa3550c1a25
rainliu67 <rainliu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2008-11-07 06:39:52 +0000

    add url_type to deal

--- vendor/plugins/engines/init.rb
+++ vendor/plugins/engines/init.rb
@@ -1,5 +1,6 @@
 # Only call Engines.init once, in the after_initialize block so that Rails
 # plugin reloading works when turned on
+require 'cached_model'
 config.after_initialize do
   Engines.init if defined? :Engines
 end

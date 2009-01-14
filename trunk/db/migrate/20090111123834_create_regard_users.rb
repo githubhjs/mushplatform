175674b7f281dc156e92d07f9415426661c399e5
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-01-14 14:45:06 +0000

    change migration create table

--- trunk/db/migrate/20090111123834_create_regard_users.rb
+++ trunk/db/migrate/20090111123834_create_regard_users.rb
@@ -1,7 +1,7 @@
 class CreateRegardUsers < ActiveRecord::Migration
 
   def self.up
-    create_table :regard_users do |t|
+    create_table :regard_users, :force => true, :options => "ENGINE=MyISAM DEFAULT CHARSET=utf8" do |t|
       t.integer :user_id
       t.integer :friend_id
       t.integer :regard_id

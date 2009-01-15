02d571ee9f532e41c0c054d24d66e96a51b25d99
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-01-15 08:14:05 +0000

    remove chmod action

--- vendor/plugins/file_column/lib/file_column.rb
+++ vendor/plugins/file_column/lib/file_column.rb
@@ -233,7 +233,7 @@ module FileColumn # :nodoc:
       else
         raise ArgumentError.new("Do not know how to handle #{file.inspect}")
       end
-      File.chmod(options[:permissions], local_file_path)
+      #File.chmod(options[:permissions], local_file_path)
       
       if options[:fix_file_extensions]
         # try to determine correct file extension and fix

cf8d7e478a3fed3ce25aa762266d107014e6a96d
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-02-15 06:24:15 +0000

    fix have no profile bug

--- trunk/app/helpers/my_space_helper.rb
+++ trunk/app/helpers/my_space_helper.rb
@@ -1,13 +1,16 @@
 module MySpaceHelper
 
   def display_blog_admin_sex
-    case current_blog_user.user_profile.sex
-    when Const::Sex_Unknown
-      '保密'
-    when Const::Sex_Woman
-      '女'
-    when Const::Sex_Man
-      '男'
+    profile = current_blog_user.user_profile
+    if profile
+      case profile.sex
+      when Const::Sex_Unknown
+        '保密'
+      when Const::Sex_Woman
+        '女'
+      when Const::Sex_Man
+        '男'
+      end
     end
   end
   

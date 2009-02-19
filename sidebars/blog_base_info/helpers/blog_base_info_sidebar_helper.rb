5caabdf616c02626db6c56d8728020aa9d9a7e56
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-02-19 05:36:53 +0000

    add blog view count when view index or blog

--- sidebars/blog_base_info/helpers/blog_base_info_sidebar_helper.rb
+++ sidebars/blog_base_info/helpers/blog_base_info_sidebar_helper.rb
@@ -4,7 +4,12 @@ module BlogBaseInfoSidebarHelper
       values = {}
       values['blogs_count'] = Blog.count('id', :conditions => "user_id = #{option[:user_id]}")
       values['comments_count'] = Comment.count('id', :conditions => "blog_user_id = #{option[:user_id]}")
-      values['hits_count'] = Blog.sum('view_count', :conditions => "user_id = #{option[:user_id]}")
+      blog_config = BlogConfig.find_by_user_id(option[:user_id])
+      if blog_config
+        values['hits_count'] = blog_config.view_count
+      else
+        values['hits_count'] = Blog.sum('view_count', :conditions => "user_id = #{option[:user_id]}")
+      end
       values['created_at'] = User.find(option[:user_id]).created_at
       values
     end

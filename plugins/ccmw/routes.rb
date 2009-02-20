2ee92f2f34469ea9bf5c00f4f35ab1383eb00f7d
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-02-20 13:45:43 +0000

    add article_categories route

--- plugins/ccmw/routes.rb
+++ plugins/ccmw/routes.rb
@@ -4,8 +4,8 @@ map.resources :links, :path_prefix => '/admin', :controller => 'admin/links'
 map.connect 'admin/tags/:action', :controller => 'admin/tags'
 map.resources :tags, :path_prefix => '/admin', :controller => 'admin/tags'
 
-map.connect 'admin/blogs/:action', :controller => 'admin/blogs'
-map.resources :blogs, :path_prefix => '/admin', :controller => 'admin/blogs'
+map.connect 'admin/article_categories/:action', :controller => 'admin/article_categories'
+map.resources :article_categories, :path_prefix => '/admin', :controller => 'admin/article_categories'
 
 map.connect 'admin/blogs/:action', :controller => 'admin/blogs'
 map.resources :blogs, :path_prefix => '/admin', :controller => 'admin/blogs'

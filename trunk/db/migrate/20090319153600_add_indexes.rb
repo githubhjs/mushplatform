821fc6ba39a437e15ffaf5a86c7b552fe6e74e61
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-03-19 07:52:49 +0000

    add index migration

---
+++ trunk/db/migrate/20090319153600_add_indexes.rb
@@ -0,0 +1,23 @@
+class AddIndexes < ActiveRecord::Migration
+  def self.up
+    add_index "blogs", :user_id
+    add_index "blogs", :published
+    add_index "blogs", :collected
+    add_index "comments", :blog_user_id
+    add_index "comments", :blog_id
+    add_index "blog_configs", :user_id
+    add_index "scriptlets", :name
+    add_index "article", :category_id
+  end
+
+  def self.down
+    remove_index "blogs", :user_id
+    remove_index "blogs", :published
+    remove_index "blogs", :collected
+    remove_index "comments", :blog_user_id
+    remove_index "comments", :blog_id
+    remove_index "blog_configs", :user_id
+    remove_index "scriptlets", :name
+    remove_index "article", :category_id
+  end
+end

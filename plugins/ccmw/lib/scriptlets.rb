8d7d492f90a297a426718a3e7b629fb1a9693cba
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-04-10 13:17:43 +0000

    add image links helper

--- plugins/ccmw/lib/scriptlets.rb
+++ plugins/ccmw/lib/scriptlets.rb
@@ -8,6 +8,7 @@ add_scriptlet_type('type_name' => 'list_articles_by_categories', 'function' => :
 add_scriptlet_type('type_name' => 'list_categories_by_category', 'function' => :list_categories_by_category)
 add_scriptlet_type('type_name' => 'list_tags_by_category', 'function' => :list_tags_by_category)
 add_scriptlet_type('type_name' => 'list_links_by_category', 'function' => :list_links_by_category)
+add_scriptlet_type('type_name' => 'list_image_links_by_category', 'function' => :list_image_links_by_category)
 add_scriptlet_type('type_name' => 'focus_images_from_links', 'function' => :focus_images_from_links)
 add_scriptlet_type('type_name' => 'years_of_journals', 'function' => :years_of_journals)
 #add_scriptlet_type('type_name' => 'list_articles', 'function' => :list_articles)

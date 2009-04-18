1f62c7c7ff6d57d735afd8bacc35f1916809d121
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-04-18 13:22:40 +0000

    add article liquid category field

--- plugins/cms/app/models/article.rb
+++ plugins/cms/app/models/article.rb
@@ -40,7 +40,7 @@ class Article < CachedModel
        excerpt = body.gsub(/<.+?>/,'').substr(0,170)
        atts['excerpt'] = excerpt
      end
-     atts['category'] = category.name
+     atts['category'] = category.name if category
      atts
   end
 

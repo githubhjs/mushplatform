bab152a1bbf07ffeaddf69d9e0654f83a0be6358
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-05-05 12:45:50 +0000

    add user search

--- trunk/plugins/authorize/app/controllers/admin/users_controller.rb
+++ trunk/plugins/authorize/app/controllers/admin/users_controller.rb
@@ -85,7 +85,7 @@ class Admin::UsersController < ApplicationController
     @users = User.find_by_user_name(params[:name])
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html { render :template => "index" }
       format.xml  { render :xml => @users }
     end
   end

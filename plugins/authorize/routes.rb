f27cb44607d044b7dfafddf57f9b20e1deaae865
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-05-05 13:13:44 +0000

    add user password modifying

--- plugins/authorize/routes.rb
+++ plugins/authorize/routes.rb
@@ -1,6 +1,5 @@
 map.namespace(:admin) do |admin|
-  admin.resources :users,:member => {:authorize_user => :post, :search => :get}
+  admin.resources :users,:member => {:authorize_user => :post}, :collection => {:search => :get}
   admin.resources :roles
   admin.resources :groups 
 end
-

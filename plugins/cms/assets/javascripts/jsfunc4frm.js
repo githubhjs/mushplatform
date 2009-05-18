edefb4bf167d1987f7fb2318980210fe206f2361
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-05-18 15:54:21 +0000

    add tags selection

---
+++ plugins/cms/assets/javascripts/jsfunc4frm.js
@@ -0,0 +1,5 @@
+function ReHth(){
+ top.document.getElementById("SmlFrm").style.height=document.getElementById("SWinWrap").scrollHeight+10+"px";
+ parent.document.getElementById("SmlWin").getElementsByTagName("h3")[0].innerHTML=document.getElementById("SWinWrap").getElementsByTagName("h3")[0].innerHTML;
+}
+

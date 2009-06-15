ca8e7d5c82ef553b3f8ae900f222bdabdc03a60c
RainLiu67 <RainLiu67@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-06-15 01:35:43 +0000

    ：<> lixj

--- trunk/config/routes.rb
+++ trunk/config/routes.rb
@@ -125,14 +125,25 @@ ActionController::Routing::Routes.draw do |map|
   map.with_options :controller => 'active' do |active|
     active.connect  '/active',:action => 'index'
     active.connect  '/active/news',        :action =>  'active_news'
-    active.connect  '/active/news',        :action =>  'active_news'
+    active.connect  '/active/arrange',        :action =>  'active_arrange'
+    active.connect  '/active/player',        :action =>  'active_player'
+    active.connect  '/active/contact',        :action =>  'active_contact'
     active.connect  '/active/login',       :action =>  'login'
     active.connect  '/active/logout',      :action =>  'logout'
+    
+    active.connect '/active/player_list', :action =>  'player_list'
+    active.connect '/active/search', :action =>  'search'
     active.connect '/active/join', :action =>  'take_part_in'
     active.connect '/active/create_comment', :action =>  'comment',:method => "post"
     active.connect '/:user_name/active', :action =>  'player'
+    active.connect '/active/blog_list', :action =>  'blog_list'
+    active.connect '/active/comment_list', :action =>  'comment_list'
+    
     active.connect '/active/vote',:action => "vote",:method => "post"
   end  
+  map.with_options :controller => 'vip' do |vip|
+    vip.connect '/vip', :action => 'index'
+  end
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"  

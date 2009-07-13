95177a4aec66b0a929b53a7434a9c14221ea8dd1
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-07-13 13:41:45 +0000

    change routes to support pagination

--- trunk/plugins/ccmw/routes.rb
+++ trunk/plugins/ccmw/routes.rb
@@ -1,13 +1,17 @@
 map.connect 'admin/links/:action', :controller => 'admin/links'
+map.connect 'admin/links/page/:page', :controller => 'admin/links', :action => 'index'
 map.resources :links, :path_prefix => '/admin', :controller => 'admin/links'
 
 map.connect 'admin/tags/:action', :controller => 'admin/tags'
+map.connect 'admin/tags/page/:page', :controller => 'admin/tags', :action => 'index'
 map.resources :tags, :path_prefix => '/admin', :controller => 'admin/tags'
 
 map.connect 'admin/article_categories/:action', :controller => 'admin/article_categories'
+map.connect 'admin/article_categories/page/:page', :controller => 'admin/article_categories', :action => 'index'
 map.resources :article_categories, :path_prefix => '/admin', :controller => 'admin/article_categories'
 
 map.connect 'admin/blogs/:action', :controller => 'admin/blogs'
+map.connect 'admin/blogs/page/:page', :controller => 'admin/blogs', :action => 'index'
 map.resources :blogs, :path_prefix => '/admin', :controller => 'admin/blogs'
 
 map.connect 'admin/blocks/:action', :controller => 'admin/blocks'

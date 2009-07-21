3333ef7e6d66cf375c775aacbdf31a430ba1bc85
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-07-21 04:09:39 +0000

    add rescue for upload file

--- vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/backends/file_system_backend.rb
+++ vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/backends/file_system_backend.rb
@@ -21,6 +21,7 @@ module Technoweenie # :nodoc:
           file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
           File.join(RAILS_ROOT, file_system_path, *partitioned_path(thumbnail_name_for(thumbnail)))
           rescue
+	  ""
           end
         end
       

5ebc8f4a07200856874c3d25dcef8ac5853f9967
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-07-21 04:03:11 +0000

    add rescue for upload file

--- trunk/vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/backends/file_system_backend.rb
+++ trunk/vendor/plugins/attachment_fu/lib/technoweenie/attachment_fu/backends/file_system_backend.rb
@@ -17,8 +17,11 @@ module Technoweenie # :nodoc:
         # Overwrite this method in your model to customize the filename.
         # The optional thumbnail argument will output the thumbnail's filename.
         def full_filename(thumbnail = nil)
+	  begin
           file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
           File.join(RAILS_ROOT, file_system_path, *partitioned_path(thumbnail_name_for(thumbnail)))
+          rescue
+          end
         end
       
         # Used as the base path that #public_filename strips off full_filename to create the public path

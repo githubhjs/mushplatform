60fbbdac0f79225a1696e540349f1a38fcfc8394
pawamaba <pawamaba@65fbdf36-ee50-0410-97a6-1fd93b61c854>
2009-08-11 14:27:44 +0000

    change article sweep

--- plugins/cms/app/models/site_sweeper.rb
+++ plugins/cms/app/models/site_sweeper.rb
@@ -1,19 +1,21 @@
+include CmsHelper
+
 class SiteSweeper < ActionController::Caching::Sweeper
-  observe Article, Channel, Template
+  observe Article #, Channel, Template
 
   def after_save(record)
-    self.class::sweep
+    self.class::sweep "#{article_permalink(record)}.html"
   end
   
   def after_destroy(record)
-    self.class::sweep
+    self.class::sweep "#{article_permalink(record)}.html"
   end
   
   def self.sweep(pattern = "*")
     cache_dir = ActionController::Base.page_cache_directory
     unless cache_dir == RAILS_ROOT+"/public"
       FileUtils.rm_r(Dir.glob(cache_dir+"/#{pattern}")) rescue Errno::ENOENT
-      RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully swept.")
+      #RAILS_DEFAULT_LOGGER.info("Cache directory '#{cache_dir}' fully swept.")
     end
   end
 end
